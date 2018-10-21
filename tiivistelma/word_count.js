061ec82c5a818b3a665d80ec0a9a9786e004432e
Jonne Pihlanen <jonne.pihlanen@gmail.com>
2018-10-21 11:21:00 +0300

    Jatkettu oikolukua ja viimeisiä lisäyksiä, lisätty aliprojektit tiivistelmälle ja seminaariesitykselle.

---
+++ tiivistelma/word_count.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const {promisify} = require('util');
+
+const readFile = promisify(fs.readFile);
+
+(async () => {
+  const file = (await readFile('tiivistelma.tex')).toString();
+  const text = file.slice(file.indexOf('Suosittelu'), file.lastIndexOf('{document}') - 4);
+  const words = text.split(' ').filter(word => word || !word.includes('cite{'));
+  console.table(words.length);
+})();
