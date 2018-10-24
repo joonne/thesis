03657bfbfde365d0880bd85cf2b5f82ffb164dae
Jonne Pihlanen <jonne.pihlanen@gmail.com>
2018-10-24 22:26:37 +0300

    Jatkettu

--- tiivistelma/word_count.js
+++ tiivistelma/word_count.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
-const {promisify} = require('util');
+const { promisify } = require('util');
 
 const readFile = promisify(fs.readFile);
 
 (async () => {
-  const file = (await readFile('tiivistelma.tex')).toString();
-  const text = file.slice(file.indexOf('Suosittelu'), file.lastIndexOf('{document}') - 4);
+  const file = await readFile('tiivistelma.tex');
+  const text = file.toString().slice(file.indexOf('Suosittelu'), 
+file.lastIndexOf('{document}') - 4);
   const words = text.split(' ').filter(word => word || !word.includes('cite{'));
   console.table(words.length);
 })();
